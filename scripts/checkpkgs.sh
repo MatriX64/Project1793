7a4b0c584d3cc4a39b9b45d29593b202aedb9907
MatriX64 <jenyakorochkov@gmail.com>
2016-11-30 00:25:19 +0300

    edit init script

--- scripts/checkpkgs.sh
+++ scripts/checkpkgs.sh
@@ -7,19 +7,19 @@
 
 USERID=""
 
-#if [ -n "$(command -v id 2> /dev/null)" ]; then
-#        USERID="$(id -u 2> /dev/null)"
-#fi
+if [ -n "$(command -v id 2> /dev/null)" ]; then
+        USERID="$(id -u 2> /dev/null)"
+fi
 
-#if [ -z "${USERID}" ] && [ -n "$(id -ru)" ]; then
-#        USERID="$(id -ru)"
-#fi
+if [ -z "${USERID}" ] && [ -n "$(id -ru)" ]; then
+        USERID="$(id -ru)"
+fi
 
-#if [ -n "${USERID}" ] && [ "${USERID}" != "0" ]; then
-#        printf "nroot\n" ; exit 1;
-#elif [ -z "${USERID}" ]; then
-#        printf "adenied\n"
-#fi
+if [ -n "${USERID}" ] && [ "${USERID}" != "0" ]; then
+        printf "nroot\n" ; exit 1;
+elif [ -z "${USERID}" ]; then
+        printf "adenied\n"
+fi
 
 sleep 3
 
